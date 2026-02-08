0ee6b1e69081a30a68f1a0136f4a6fddbcf25d4f
Thiago Anjos <anjos.tg@gmail.com>
2026-02-08 23:47:42 +0000

    feat: Add Unidade de Medida doctype with initial structure and tests

---
+++ pedidoja_app/cadastro_básico/doctype/unidade_de_medida/unidade_de_medida.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Thiago Anjos and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Unidade de Medida", {
+// 	refresh(frm) {
+
+// 	},
+// });
